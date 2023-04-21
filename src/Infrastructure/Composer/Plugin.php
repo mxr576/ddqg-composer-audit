394cc227346e21c58aeb555d2dca29683837e4c9
Dezső BICZÓ <mxr576@gmail.com>
2023-04-21 21:24:48 +0200

    Display plugin below Composer 2.4.0
    Closes https://github.com/mxr576/ddqg-composer-audit/issues/1

--- src/Infrastructure/Composer/Plugin.php
+++ src/Infrastructure/Composer/Plugin.php
@@ -17,18 +17,30 @@ use mxr576\ddqgComposerAudit\Infrastructure\Ddqg\UnsupportedPackageVersionsFromL
  */
 final class Plugin implements PluginInterface
 {
+    public const PACKAGE_NAME = 'mxr576/ddqg-composer-audit';
+
     /**
      * {@inheritDoc}
      */
     public function activate(Composer $composer, IOInterface $io): void
     {
+        // This is a runtime check instead of a dependency constraint to allow
+        // installation of this package on projects where composer/composer also
+        // installed as a project dependency (but a global version used instead).
+        // @see https://github.com/mxr576/ddqg-composer-audit/issues/1
+        if (version_compare($composer::VERSION, '2.4.0', '<')) {
+            $io->warning(sprintf('%s is disabled because audit command is only available since Composer 2.4.0. Your version is: %s.', self::PACKAGE_NAME, $composer::VERSION));
+
+            return;
+        }
         $composer->getRepositoryManager()->prependRepository(
             new DdqgComposerAuditRepository(
                 [
                   new UnsupportedPackageVersionsFromLatestDdqgBuild($composer->getLoop()->getHttpDownloader()),
                   new InsecurePackageVersionsFromLatestDdqgBuild($composer->getLoop()->getHttpDownloader()),
                 ],
-                new VersionParser()
+                new VersionParser(),
+                $io
             )
         );
     }
