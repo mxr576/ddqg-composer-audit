mxr576/ddqg-composer-audit

commit 51d1211d5ab888a253cabda144c41c040bd57f9b

Drupal 10 compatibility check must respect --locked command parameter
Also introduce our own domain concept for an installed package repository.

src/Presentation/Composer/Plugin.php
@@ -21,6 +21,8 @@ use Composer\IO\IOInterface;
 use Composer\IO\NullIO;
 use Composer\Package\Version\VersionParser;
 use Composer\Plugin\PluginInterface;
+use Composer\Repository\InstalledRepository;
+use mxr576\ddqgComposerAudit\Infrastructure\Composer\InstalledPackagesReadOnlyRepository;
 use mxr576\ddqgComposerAudit\Presentation\Composer\Repository\ComposerAuditRepository;
 use mxr576\ddqgComposerAudit\Supportive\Adapter\Composer\DeprecatedPackageWasIgnoredAdapter;
 use mxr576\ddqgComposerAudit\Supportive\Adapter\Composer\UnsupportedPackageWasIgnoredAdapter;
@@ -65,6 +67,7 @@ final class Plugin implements PluginInterface, EventSubscriberInterface
         }
 
         $with_dev_dependencies = false;
+        $locked_dependencies = false;
         // This is not great, not terrible... the only benefit of this is
         // probably reducing the amount of objects build by the
         // LockerRepository.
@@ -75,7 +78,16 @@ final class Plugin implements PluginInterface, EventSubscriberInterface
                 $ro_io->getProperty('input')->setAccessible(true);
                 assert($ro_io->getProperty('input')->getValue($io) instanceof InputInterface);
                 $with_dev_dependencies = str_contains((string) $ro_io->getProperty('input')->getValue($io), '--no-dev');
+                $locked_dependencies = str_contains((string) $ro_io->getProperty('input')->getValue($io), '--locked');
             }
+        }
+
+        // @todo Fix layering rules.
+        if ($locked_dependencies) {
+            $installed_packages_repository = InstalledPackagesReadOnlyRepository::fromLocker($composer->getLocker(), $with_dev_dependencies);
+        } else {
+            $composer_installed_repository = new InstalledRepository([$composer->getRepositoryManager()->getLocalRepository()]);
+            $installed_packages_repository = $with_dev_dependencies ? InstalledPackagesReadOnlyRepository::fromInstalledPackages($composer_installed_repository) : InstalledPackagesReadOnlyRepository::fromInstalledRequiredPackages($composer_installed_repository, $composer->getPackage());
         }
 
         $version_parser = new VersionParser();
@@ -111,7 +123,7 @@ final class Plugin implements PluginInterface, EventSubscriberInterface
         );
         $composer->getRepositoryManager()->prependRepository(
             new ComposerAuditRepository(
-                (new FindNonDrupal10CompatiblePackagesFactoryFromComposerRuntimeDependencies($composer->getPackage(), $composer->getLocker()->getLockedRepository($with_dev_dependencies), $version_parser))->create(),
+                (new FindNonDrupal10CompatiblePackagesFactoryFromComposerRuntimeDependencies($composer->getPackage(), $installed_packages_repository, $version_parser))->create(),
                 $io
             )
         );

src/Supportive/Factory/FindNonDrupal10CompatiblePackagesFactoryFromComposerRuntimeDependencies.php
@@ -15,9 +15,9 @@ declare(strict_types=1);
 namespace mxr576\ddqgComposerAudit\Supportive\Factory;
 
 use Composer\Package\RootPackageInterface;
-use Composer\Repository\LockArrayRepository;
 use Composer\Semver\VersionParser;
 use mxr576\ddqgComposerAudit\Application\PackageFinder\FindNonDrupal10CompatiblePackages;
+use mxr576\ddqgComposerAudit\Infrastructure\Composer\InstalledPackagesReadOnlyRepository;
 use mxr576\ddqgComposerAudit\Infrastructure\Composer\NonDrupal10CompatiblePackageFinderConfigurationProvider;
 use mxr576\ddqgComposerAudit\Supportive\Infrastructure\Composer\NonDrupal10CompatiblePackageVersionsProviderFromComposerLock;
 
@@ -28,7 +28,7 @@ final class FindNonDrupal10CompatiblePackagesFactoryFromComposerRuntimeDependenc
 {
     public function __construct(
         private readonly RootPackageInterface $rootPackage,
-        private readonly LockArrayRepository $lockRepository,
+        private readonly InstalledPackagesReadOnlyRepository $installedPackageFinder,
         private readonly VersionParser $versionParser,
     ) {
     }
@@ -36,7 +36,7 @@ final class FindNonDrupal10CompatiblePackagesFactoryFromComposerRuntimeDependenc
     public function create(): FindNonDrupal10CompatiblePackages
     {
         return new FindNonDrupal10CompatiblePackages(
-            new NonDrupal10CompatiblePackageVersionsProviderFromComposerLock($this->lockRepository, $this->versionParser),
+            new NonDrupal10CompatiblePackageVersionsProviderFromComposerLock($this->installedPackageFinder, $this->versionParser),
             $this->versionParser,
             new NonDrupal10CompatiblePackageFinderConfigurationProvider($this->rootPackage)
         );

phpstan-baseline.neon
@@ -17,10 +17,5 @@ parameters:
 
 		-
 			message: "#^Cannot cast Symfony\\\\Component\\\\Console\\\\Input\\\\InputInterface to string\\.$#"
-			count: 1
-			path: src/Presentation/Composer/Plugin.php
-
-		-
-			message: "#^Method mxr576\\\\ddqgComposerAudit\\\\Presentation\\\\Composer\\\\Plugin\\:\\:activate\\(\\) throws checked exception RuntimeException but it's missing from the PHPDoc @throws tag\\.$#"
-			count: 1
+			count: 2
 			path: src/Presentation/Composer/Plugin.php

src/Domain/InstalledPackages/InstalledPackagesReadOnlyRepository.php
@@ -1,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2023-2024 Dezső Biczó
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/mxr576/ddqg-composer-audit/LICENSE.md
+ *
+ */
+
+namespace mxr576\ddqgComposerAudit\Domain\InstalledPackages;
+
+use Composer\Package\PackageInterface;
+use Composer\Semver\Constraint\ConstraintInterface;
+
+interface InstalledPackagesReadOnlyRepository
+{
+    public function findByName(string $package_name, string|ConstraintInterface $version): ?PackageInterface;
+
+    /**
+     * @return \Composer\Package\PackageInterface[]
+     */
+    public function getPackages(): array;
+}

src/Infrastructure/Composer/InstalledPackagesReadOnlyRepository.php
@@ -1,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2023-2024 Dezső Biczó
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/mxr576/ddqg-composer-audit/LICENSE.md
+ *
+ */
+
+namespace mxr576\ddqgComposerAudit\Infrastructure\Composer;
+
+use Composer\Package\Locker;
+use Composer\Package\PackageInterface;
+use Composer\Package\RootPackageInterface;
+use Composer\Repository\InstalledRepository;
+use Composer\Repository\LockArrayRepository;
+use Composer\Repository\RepositoryUtils;
+use Composer\Semver\Constraint\ConstraintInterface;
+use mxr576\ddqgComposerAudit\Domain\InstalledPackages\InstalledPackagesReadOnlyRepository as InstalledPackagesReadOnlyRepositoryConstract;
+
+/**
+ * @internal
+ */
+final class InstalledPackagesReadOnlyRepository implements InstalledPackagesReadOnlyRepositoryConstract
+{
+    /**
+     * @var callable(string, string|ConstraintInterface): ?PackageInterface
+     */
+    private $packageFinder;
+
+    /**
+     * @var callable(): PackageInterface[]
+     */
+    private $packageGetter;
+
+    /**
+     * Constructs a new object.
+     *
+     * @param callable(string $package_name, string|ConstraintInterface $version): ?PackageInterface $packageFinder
+     * @param callable(): PackageInterface[] $packageGetter
+     */
+    private function __construct(
+        $packageFinder,
+        $packageGetter
+    ) {
+        $this->packageGetter = $packageGetter;
+        $this->packageFinder = $packageFinder;
+    }
+
+    public static function fromLocker(Locker $locker, bool $withDevDependencies): self
+    {
+        if (!$locker->isLocked()) {
+            throw new \InvalidArgumentException('Valid composer.json and composer.lock files are required to run this command with --locked');
+        }
+
+        try {
+            return self::fromLockedPackages($locker->getLockedRepository($withDevDependencies));
+        } catch (\RuntimeException $e) {
+            throw new \InvalidArgumentException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+
+    public static function fromLockedPackages(LockArrayRepository $lockArrayRepository): self
+    {
+        return new self(
+            static fn (string $package_name, string|ConstraintInterface $version): ?PackageInterface => $lockArrayRepository->findPackage($package_name, $version),
+            /** @return PackageInterface[] */
+            static fn (): array => $lockArrayRepository->getPackages(),
+        );
+    }
+
+    public static function fromInstalledPackages(InstalledRepository $installedRepository): self
+    {
+        return new self(
+            static fn (string $package_name, string|ConstraintInterface $version): ?PackageInterface => $installedRepository->findPackage($package_name, $version),
+            /** @return PackageInterface[] */
+            static fn (): array => $installedRepository->getPackages(),
+        );
+    }
+
+    public static function fromInstalledRequiredPackages(InstalledRepository $installedRepository, RootPackageInterface $rootPackage): self
+    {
+        return new self(
+            static function (string $package_name, string|ConstraintInterface $version) use (
+                $installedRepository, $rootPackage
+            ): ?PackageInterface {
+                $package = $installedRepository->findPackage($package_name, $version);
+                if (null === $package) {
+                    return null;
+                }
+
+                $tmp = RepositoryUtils::filterRequiredPackages([$package], $rootPackage);
+                if ([] === $tmp) {
+                    return null;
+                }
+
+                return $package;
+            },
+            /** @return PackageInterface[] */
+            static fn (): array => $installedRepository->getPackages(),
+        );
+    }
+
+    public function findByName(string $package_name, string|ConstraintInterface $version): ?PackageInterface
+    {
+        return ($this->packageFinder)($package_name, $version);
+    }
+
+    public function getPackages(): array
+    {
+        return ($this->packageGetter)();
+    }
+}

src/Supportive/Adapter/Composer/Psr14EventDispatcherAdapterForComposer.php
@@ -26,7 +26,7 @@ use Psr\EventDispatcher\StoppableEventInterface;
  */
 final class Psr14EventDispatcherAdapterForComposer implements EventDispatcherInterface
 {
-    public function __construct(private ComposerEventDispatcher $dispatcher)
+    public function __construct(private readonly ComposerEventDispatcher $dispatcher)
     {
     }
 

src/Supportive/Infrastructure/Composer/NonDrupal10CompatiblePackageVersionsProviderFromComposerLock.php
@@ -14,10 +14,10 @@ declare(strict_types=1);
 
 namespace mxr576\ddqgComposerAudit\Supportive\Infrastructure\Composer;
 
-use Composer\Repository\LockArrayRepository;
 use Composer\Semver\Constraint\Constraint;
 use Composer\Semver\VersionParser;
 use mxr576\ddqgComposerAudit\Domain\PackageVersionsProvider\NonDrupal10CompatiblePackageVersionsProvider;
+use mxr576\ddqgComposerAudit\Infrastructure\Composer\InstalledPackagesReadOnlyRepository;
 
 /**
  * @internal
@@ -42,7 +42,7 @@ final class NonDrupal10CompatiblePackageVersionsProviderFromComposerLock impleme
       'drupal/core-dev',
     ];
 
-    public function __construct(private readonly LockArrayRepository $lockRepository, private readonly VersionParser $versionParser)
+    public function __construct(private readonly InstalledPackagesReadOnlyRepository $installedPackagesRepository, private readonly VersionParser $versionParser)
     {
     }
 
@@ -50,7 +50,8 @@ final class NonDrupal10CompatiblePackageVersionsProviderFromComposerLock impleme
     {
         $result = [];
         $d10_compatible_constraint = new Constraint('>=', $this->versionParser->normalize('10.0.0'));
-        foreach ($this->lockRepository->getPackages() as $package) {
+
+        foreach ($this->installedPackagesRepository->getPackages() as $package) {
             if (!in_array($package->getName(), $package_names, true)) {
                 continue;
             }

commit 719260204258b8912c48c35df1e1794d36f5c924

Address layering rule issues introduced in 51d1211

src/Presentation/Composer/Plugin.php
@@ -22,13 +22,12 @@ use Composer\IO\NullIO;
 use Composer\Package\Version\VersionParser;
 use Composer\Plugin\PluginInterface;
 use Composer\Repository\InstalledRepository;
-use mxr576\ddqgComposerAudit\Infrastructure\Composer\InstalledPackagesReadOnlyRepository;
 use mxr576\ddqgComposerAudit\Presentation\Composer\Repository\ComposerAuditRepository;
 use mxr576\ddqgComposerAudit\Supportive\Adapter\Composer\DeprecatedPackageWasIgnoredAdapter;
 use mxr576\ddqgComposerAudit\Supportive\Adapter\Composer\UnsupportedPackageWasIgnoredAdapter;
 use mxr576\ddqgComposerAudit\Supportive\Factory\FindDeprecatedPackagesFactoryFromComposerRuntimeDependencies;
 use mxr576\ddqgComposerAudit\Supportive\Factory\FindInsecurePackagesFactoryFromComposerRuntimeDependencies;
-use mxr576\ddqgComposerAudit\Supportive\Factory\FindNonDrupal10CompatiblePackagesFactoryFromComposerRuntimeDependencies;
+use mxr576\ddqgComposerAudit\Supportive\Factory\FindNonDrupal10CompatiblePackagesFactory;
 use mxr576\ddqgComposerAudit\Supportive\Factory\FindUnsupportedPackagesFactoryFromComposerRuntimeDependencies;
 use Symfony\Component\Console\Input\InputInterface;
 
@@ -82,15 +81,15 @@ final class Plugin implements PluginInterface, EventSubscriberInterface
             }
         }
 
-        // @todo Fix layering rules.
+        $version_parser = new VersionParser();
+
+        $d10_factory = new FindNonDrupal10CompatiblePackagesFactory($composer->getPackage(), $version_parser);
         if ($locked_dependencies) {
-            $installed_packages_repository = InstalledPackagesReadOnlyRepository::fromLocker($composer->getLocker(), $with_dev_dependencies);
+            $d10_non_compatible_finder = $with_dev_dependencies ? $d10_factory->inLockedDependencies($composer->getLocker()) : $d10_factory->inLockedRequiredDependencies($composer->getLocker());
         } else {
-            $composer_installed_repository = new InstalledRepository([$composer->getRepositoryManager()->getLocalRepository()]);
-            $installed_packages_repository = $with_dev_dependencies ? InstalledPackagesReadOnlyRepository::fromInstalledPackages($composer_installed_repository) : InstalledPackagesReadOnlyRepository::fromInstalledRequiredPackages($composer_installed_repository, $composer->getPackage());
+            $d10_non_compatible_finder = $with_dev_dependencies ? $d10_factory->inInstalledDependencies(new InstalledRepository([$composer->getRepositoryManager()->getLocalRepository()])) : $d10_factory->inInstalledRequiredDependencies(new InstalledRepository([$composer->getRepositoryManager()->getLocalRepository()]));
         }
 
-        $version_parser = new VersionParser();
         // Composer currently only displays advisories from one repository for
         // a package. If multiple ones provides advisories only the first one
         // is visible.
@@ -123,7 +122,7 @@ final class Plugin implements PluginInterface, EventSubscriberInterface
         );
         $composer->getRepositoryManager()->prependRepository(
             new ComposerAuditRepository(
-                (new FindNonDrupal10CompatiblePackagesFactoryFromComposerRuntimeDependencies($composer->getPackage(), $installed_packages_repository, $version_parser))->create(),
+                $d10_non_compatible_finder,
                 $io
             )
         );

src/Supportive/Factory/FindNonDrupal10CompatiblePackagesFactory.php
@@ -1,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2023-2024 Dezső Biczó
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/mxr576/ddqg-composer-audit/LICENSE.md
+ *
+ */
+
+namespace mxr576\ddqgComposerAudit\Supportive\Factory;
+
+use Composer\Package\Locker;
+use Composer\Package\RootPackageInterface;
+use Composer\Repository\InstalledRepository;
+use Composer\Semver\VersionParser;
+use mxr576\ddqgComposerAudit\Application\PackageFinder\FindNonDrupal10CompatiblePackages;
+use mxr576\ddqgComposerAudit\Infrastructure\Composer\InstalledPackagesReadOnlyRepository;
+use mxr576\ddqgComposerAudit\Infrastructure\Composer\NonDrupal10CompatiblePackageFinderConfigurationProvider;
+use mxr576\ddqgComposerAudit\Supportive\Infrastructure\Composer\NonDrupal10CompatiblePackageVersionsProviderFromComposerLock;
+
+/**
+ * @internal
+ */
+final class FindNonDrupal10CompatiblePackagesFactory
+{
+    public function __construct(
+        private readonly RootPackageInterface $rootPackage,
+        private readonly VersionParser $versionParser,
+    ) {
+    }
+
+    public function inInstalledDependencies(InstalledRepository $installedRepository): FindNonDrupal10CompatiblePackages
+    {
+        return new FindNonDrupal10CompatiblePackages(
+            new NonDrupal10CompatiblePackageVersionsProviderFromComposerLock(InstalledPackagesReadOnlyRepository::fromInstalledPackages($installedRepository), $this->versionParser),
+            $this->versionParser,
+            new NonDrupal10CompatiblePackageFinderConfigurationProvider($this->rootPackage)
+        );
+    }
+
+    public function inInstalledRequiredDependencies(InstalledRepository $installedRepository): FindNonDrupal10CompatiblePackages
+    {
+        return new FindNonDrupal10CompatiblePackages(
+            new NonDrupal10CompatiblePackageVersionsProviderFromComposerLock(InstalledPackagesReadOnlyRepository::fromInstalledRequiredPackages($installedRepository, $this->rootPackage), $this->versionParser),
+            $this->versionParser,
+            new NonDrupal10CompatiblePackageFinderConfigurationProvider($this->rootPackage)
+        );
+    }
+
+    public function inLockedDependencies(Locker $locker): FindNonDrupal10CompatiblePackages
+    {
+        return new FindNonDrupal10CompatiblePackages(
+            new NonDrupal10CompatiblePackageVersionsProviderFromComposerLock(InstalledPackagesReadOnlyRepository::fromLocker($locker, true), $this->versionParser),
+            $this->versionParser,
+            new NonDrupal10CompatiblePackageFinderConfigurationProvider($this->rootPackage)
+        );
+    }
+
+    public function inLockedRequiredDependencies(Locker $locker): FindNonDrupal10CompatiblePackages
+    {
+        return new FindNonDrupal10CompatiblePackages(
+            new NonDrupal10CompatiblePackageVersionsProviderFromComposerLock(InstalledPackagesReadOnlyRepository::fromLocker($locker, false), $this->versionParser),
+            $this->versionParser,
+            new NonDrupal10CompatiblePackageFinderConfigurationProvider($this->rootPackage)
+        );
+    }
+}

src/Supportive/Factory/FindNonDrupal10CompatiblePackagesFactoryFromComposerRuntimeDependencies.php
@@ -1,44 +1,0 @@
-<?php
-
-declare(strict_types=1);
-
-/**
- * Copyright (c) 2023-2024 Dezső Biczó
- *
- * For the full copyright and license information, please view
- * the LICENSE.md file that was distributed with this source code.
- *
- * @see https://github.com/mxr576/ddqg-composer-audit/LICENSE.md
- *
- */
-
-namespace mxr576\ddqgComposerAudit\Supportive\Factory;
-
-use Composer\Package\RootPackageInterface;
-use Composer\Semver\VersionParser;
-use mxr576\ddqgComposerAudit\Application\PackageFinder\FindNonDrupal10CompatiblePackages;
-use mxr576\ddqgComposerAudit\Infrastructure\Composer\InstalledPackagesReadOnlyRepository;
-use mxr576\ddqgComposerAudit\Infrastructure\Composer\NonDrupal10CompatiblePackageFinderConfigurationProvider;
-use mxr576\ddqgComposerAudit\Supportive\Infrastructure\Composer\NonDrupal10CompatiblePackageVersionsProviderFromComposerLock;
-
-/**
- * @internal
- */
-final class FindNonDrupal10CompatiblePackagesFactoryFromComposerRuntimeDependencies
-{
-    public function __construct(
-        private readonly RootPackageInterface $rootPackage,
-        private readonly InstalledPackagesReadOnlyRepository $installedPackageFinder,
-        private readonly VersionParser $versionParser,
-    ) {
-    }
-
-    public function create(): FindNonDrupal10CompatiblePackages
-    {
-        return new FindNonDrupal10CompatiblePackages(
-            new NonDrupal10CompatiblePackageVersionsProviderFromComposerLock($this->installedPackageFinder, $this->versionParser),
-            $this->versionParser,
-            new NonDrupal10CompatiblePackageFinderConfigurationProvider($this->rootPackage)
-        );
-    }
-}