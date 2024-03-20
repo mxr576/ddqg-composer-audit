833cd73742688dbb6adc89245555c56cac56aae4
Dezső BICZÓ <mxr576@gmail.com>
2024-03-20 22:12:13 +0100

    Fix deprecation error message trigger logic

--- src/Domain/PackageIgnore/PackageIgnoreRuleOptimizer.php
+++ src/Domain/PackageIgnore/PackageIgnoreRuleOptimizer.php
@@ -27,16 +27,25 @@ trait PackageIgnoreRuleOptimizer
         $rules = $provider->getIgnoreRules();
         /** @var \mxr576\ddqgComposerAudit\Domain\PackageIgnore\PackageIgnoreRule[] $rules */
         $rules = $rules instanceof \Traversable ? iterator_to_array($rules, false) : $rules;
-        if ([] !== $rules) {
-            trigger_error("Since 1.1.0 DDQG Composer Audit plugin's ignore features are deprecated, use Composer's built-in audit ignore feature instead. https://getcomposer.org/doc/06-config.md#ignore", E_USER_DEPRECATED);
-        }
+        $ignore_package_by_installed_version_of_other_package_counter = 0;
         /** @var \ArrayObject<string,array<\mxr576\ddqgComposerAudit\Domain\PackageIgnore\PackageIgnoreRule>> $tmp */
         $tmp = array_reduce($rules,
-            static function (\ArrayObject $carry, PackageIgnoreRule $item) {
+            static function (\ArrayObject $carry, PackageIgnoreRule $item) use (&$ignore_package_by_installed_version_of_other_package_counter) {
+                // This type of ignore rule can be only created programmatically at
+                // this moment, so we can assume it was added by the package
+                // maintainer(s).
+                if ($item instanceof IgnorePackageByInstalledVersionOfOtherPackage) {
+                    ++$ignore_package_by_installed_version_of_other_package_counter;
+                }
+
                 $carry[$item->getPackageName()][] = $item;
 
                 return $carry;
             }, new \ArrayObject());
+
+        if (count($tmp) !== $ignore_package_by_installed_version_of_other_package_counter) {
+            trigger_error("Since 1.1.0 DDQG Composer Audit plugin's ignore features are deprecated, use Composer's built-in audit ignore feature instead. https://getcomposer.org/doc/06-config.md#ignore", E_USER_DEPRECATED);
+        }
 
         return $tmp->getArrayCopy();
     }
